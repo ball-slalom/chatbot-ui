2517076af50b878766222fea232347e07bb09438
Chris Ball <chris.ball@slalom.com>
2023-11-10 07:14:42 -0800

    adds gpt-4-1106-preview option and 3.5-1106

--- types/openai.ts
+++ types/openai.ts
@@ -9,20 +9,28 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5_1106 = 'gpt-3.5-turbo-1106',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
+  GPT_4_1106 = 'gpt-4-1106-preview',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_4_1106;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
+    name: 'GPT-3.5-turbo',
     maxLength: 12000,
     tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_1106]: {
+    id: OpenAIModelID.GPT_3_5_1106,
+    name: 'GPT-3.5-turbo-1106',
+    maxLength: 12000,
+    tokenLimit: 16000,
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
@@ -41,5 +49,11 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-4-32K',
     maxLength: 96000,
     tokenLimit: 32000,
+  },
+  [OpenAIModelID.GPT_4_1106]: {
+    id: OpenAIModelID.GPT_4_1106,
+    name: 'GPT-4-1106-preview',
+    maxLength: 96000,
+    tokenLimit: 128000,
   },
 };
